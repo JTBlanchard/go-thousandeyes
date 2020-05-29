dbf55b22d3531e29aef60c98f4e4f795ef93c085
Joshua Blanchard <joshua.blanchard@gmail.com>
2020-05-29 08:22:10 -0400

    Set Type to blank on Label create

--- group_label.go
+++ group_label.go
@@ -7,12 +7,12 @@ type GroupLabels []GroupLabel
 
 // GroupLabel - label
 type GroupLabel struct {
-	Name    string      `json:"name,omitempty"`
-	GroupID int         `json:"groupId,omitempty"`
-	BuiltIn int         `json:"builtin,omitempty"`
-	Type    string      `json:"type,omitempty"`
-	Agents  []Agent     `json:"agents,omitempty"`
-	Tests   interface{} `json:"tests,omitempty"`
+	Name    string        `json:"name,omitempty"`
+	GroupID int           `json:"groupId,omitempty"`
+	BuiltIn int           `json:"builtin,omitempty"`
+	Type    string        `json:"type,omitempty"`
+	Agents  []Agent       `json:"agents,omitempty"`
+	Tests   []GenericTest `json:"tests,omitempty"`
 }
 
 // GetGroupLabels - Get labels
@@ -59,6 +59,9 @@ func (c *Client) GetGroupLabel(id int) (*GroupLabel, error) {
 // CreateGroupLabel - Create label
 func (c Client) CreateGroupLabel(a GroupLabel) (*GroupLabel, error) {
 	path := fmt.Sprintf("/groups/%s/new", a.Type)
+	// Now we must set Type to blank.  Because even though it's required to know the submit path,
+	// TE will return an error if we also submit it a part of the object.
+	a.Type = ""
 	resp, err := c.post(path, a, nil)
 	if err != nil {
 		return nil, err
